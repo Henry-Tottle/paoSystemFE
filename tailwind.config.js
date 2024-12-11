a042837e4d460364cca55a0c4921e67306396aca
Henry Tottle <henry@Henrys-MacBook-Air.local>
2024-12-11 23:10:34 +0000

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/*.{html,jsx}", "./src/Components/**/*.{html,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
