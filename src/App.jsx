b67a0752123fdf3cc239f6ce376a800692b139bf
Henry Tottle <henry@Henrys-MacBook-Air.local>
2024-12-16 11:03:59 +0000

    componentised some bits

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-
+import Button from "./Components/Button/index.jsx";
+import HomeBody from "./Components/HomeBody/index.jsx";
 
 
 function App() {
@@ -10,16 +11,10 @@ function App() {
                 <h1 className='text-center bg-cyan-300 mx-72 my-10 py-5 rounded-3xl'>P.A.O Builder</h1>
             </div>
             <div className='my-12 flex justify-center'>
-                <button className='bg-amber-500 border-black mx-3'>Your Build</button>
-                <button className='bg-amber-500 border-2 mx-3'>Number Converter</button>
+                <Button text='Your Build'/>
+                <Button text='Number Converter'/>
             </div>
-            <div>
-                <p>
-                    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-
-
-                </p>
-            </div>
+            <HomeBody/>
         </div>
     </>
   )
---
+++ src/Components/Button/index.jsx
@@ -0,0 +1,10 @@
+function Button ({text}) {
+    return (
+        <>
+            <button className='bg-amber-500 border-black mx-3'>{text}</button>
+        </>
+    )
+}
+
+export default Button
+
